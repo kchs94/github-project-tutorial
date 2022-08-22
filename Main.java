f2681e68e9519309ac13a483c8d046a817aa54f9
kchs94 <kchs94@gmail.com>
2022-08-22 14:45:15 +0900

    feat: 기능1의 세부 기능2

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("기능1의 세부 기능 1완성");
+        System.out.println("기능1의 세부 기능 2완성");
     }
 }
